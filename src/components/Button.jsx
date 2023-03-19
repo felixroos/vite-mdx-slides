b4ede794da4a53bfdb4d8559823e3bbba733cf75
Felix Roos <flix91@gmail.com>
2023-03-19 22:44:29 +0100

    add not-prose class to button + esnext target

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ children, ...rest }) {
   return (
-    <button className="px-2 bg-blue-500 rounded-md" {...rest}>
+    <button className="p-2 bg-blue-500 rounded-md not-prose" {...rest}>
       {children}
     </button>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [react(), mdx()],
 });
